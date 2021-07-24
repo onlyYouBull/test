91b8ad904eeab512a5f58700633af36e27fc5226
onlyYouBull <s15210815016@gmail.com>
2021-07-24 15:46:58 +0800

    update mmm class object

--- src/main/java/com/sun/mmm.java
+++ src/main/java/com/sun/mmm.java
@@ -3,5 +3,6 @@ package com.sun;
 public class mmm {
     public static void main(String[] args) {
         System.out.println("you are right ,I will be happy");
+        System.out.println("don't worry ,maybe I make a mistake ");
     }
 }
